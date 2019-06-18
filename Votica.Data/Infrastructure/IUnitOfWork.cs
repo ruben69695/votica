9abeddfb5f28f239971d55766cd7b248231e0716
Miguel Ortega <ortega.miguelm@gmail.com>
2019-06-18 12:32:28 +0200

    Create UnitOfWork interface working with generic repository

---
+++ Votica.Data/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Votica.Data.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+
+        IRepositoryBase<TEntity> GetRepository<TEntity>() where TEntity : class;
+        void Commit();
+        Task<int> CommitAsync();
+
+    }
+}
